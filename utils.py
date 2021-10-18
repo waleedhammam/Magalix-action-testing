90e7b7e51f7e9be27ee54604dd825c9add0f791c
Sarah Boctor <boctor.sarah@gmail.com>
2021-10-18 18:53:06 +0200

    Add branch and environment to slack message

--- utils.py
+++ utils.py
@@ -8,13 +8,17 @@ def read_result():
 
 
 def send_slack_message(branch: str, slack_channel: str, is_passed: bool):
+    status = "SUCCESS" if is_passed else "FAILURE"
+    env = "PROD" if branch == "main" else "DEV"
+
+    message = f"{status}: E2E Violations Prevention Test on\n{env}: `{branch}`"
     payload = {
         "type": "mrkdwn",
         "attachments": [
             {
-                "title": "E2E Violations Prevention Test",
-                "fields": [{"title": "Branch", "value": branch}],
+                "fields": [{"value": message, "short": "false"}],
                 "color": "good" if is_passed else "danger",
+                 "ts":  "timestamp"
             }
         ],
     }
